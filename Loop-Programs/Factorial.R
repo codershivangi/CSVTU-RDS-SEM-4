9189ab98853448eca494061f96240716f34185c7
Shivangi <159949628+codershivangi@users.noreply.github.com>
2025-03-17 07:41:25 -0700

    Add files via upload

--- Loop-Programs/Factorial.R
+++ Loop-Programs/Factorial.R
@@ -1,1 +1,12 @@
-
+#Write a function that calculates the factorial of a number using a loop
+myfact <- function(num) {
+  fact <- 1
+  if(num<=1)
+    return(fact)
+  for(i in 2:num){
+    fact <- fact*i
+  }
+  return(fact)
+}
+myfact(5)
+myfact(4)
